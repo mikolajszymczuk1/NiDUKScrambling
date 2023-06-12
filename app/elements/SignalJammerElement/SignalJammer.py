754624c1c3eb0fe90554cd31b605882d4c20b05d
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-06-12 09:51:01 +0200

    Start working on signal jamming process

--- app/elements/SignalJammerElement/SignalJammer.py
+++ app/elements/SignalJammerElement/SignalJammer.py
@@ -2,13 +2,21 @@ from simpy.events import Process
 from typing import Generator
 from ..PipelineBlockElement.PipelineBlock import PipelineBlock
 from ..DescramblerElement.Descrambler import Descrambler
+from enums.EnumBitState import EnumBitState
 
 class SignalJammer(PipelineBlock):
     # Run env process
     def run_process(self) -> Generator[Process, None, None]:
         print(f'Passing the signal through the interfering channel:\n - {self.data}')
-        descrambler = Descrambler(self.env, self.data)
+        descrambler = Descrambler(self.env, self.jamming_channel())
         yield self.env.process(descrambler.run_process())
 
-    def jamming_channel(self) -> None:
-        pass
+    def jamming_channel(self) -> list[int]:
+        """  """
+
+        sets_of_bits = []
+        for bit in self.data:
+            if bit == EnumBitState.HIGH.value:
+                pass
+
+        return self.data
---
+++ app/enums/EnumBitState.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class EnumBitState(Enum):
+    HIGH = 1
+    LOW = 0

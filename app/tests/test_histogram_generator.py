2d919b3899561da8f56184cf04bcf5832fe10d5f
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2023-06-14 06:37:46 +0200

    Created tests for utils functions

---
+++ app/tests/test_histogram_generator.py
@@ -0,0 +1,28 @@
+import os
+from conf.output_conf import OUTPUT_DIR
+from utils.histogram_generator import create_histogram, load_signal_from_file
+
+class TestHistogramGenerator:
+    @classmethod
+    def setup_class() -> None:
+        if not os.path.exists(OUTPUT_DIR):
+            os.makedirs(OUTPUT_DIR)
+
+    @classmethod
+    def teardown_class() -> None:
+        os.remove(os.path.join(OUTPUT_DIR, 'histogram_test.png'))
+        os.remove(os.path.join(OUTPUT_DIR, 'out_test_signal.txt'))
+
+    def test_create_histogram(self) -> None:
+        test_data: list[int] = [1, 0, 1, 1]
+        output_image: str = os.path.join(OUTPUT_DIR, 'histogram_test.png')
+        create_histogram(test_data, 'title', output_image)
+        assert os.path.exists(output_image)
+
+    def test_load_signal_from_file(self) -> None:
+        test_signal: str = os.path.join(OUTPUT_DIR, 'out_test_signal.txt')
+        with open(test_signal, 'w') as file:
+            file.write('1001101')
+
+        signal: list[int] = load_signal_from_file(test_signal)
+        assert signal == [1, 0, 0, 1, 1, 0, 1]
